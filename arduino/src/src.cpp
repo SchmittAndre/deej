ff4a0842cf74acfc69eb81dadc372782ce3a4491
Scalaqua <andre1502@gmx.de>
2024-07-20 20:18:51 +0200

    get motorised fader working
    get fader working
    used onboard neopixel to indicate driving fader

--- arduino/src/src.cpp
+++ arduino/src/src.cpp
@@ -1,10 +1,26 @@
-const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
-const int NUM_BUTTONS = 6;
-const int buttonInputs[NUM_BUTTONS] = {11,10,9,6,5};
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define LED_COUNT   1
+#define HYSTERESIS  25
+#define LED_PIN     8
+
+const int NUM_SLIDERS = 1;
+const int analogInputs[NUM_SLIDERS] = {A0}; //, A1, A2, A3, A4};
+const int NUM_BUTTONS = 1;
+const int buttonInputs[NUM_BUTTONS] = {11};//,10,9,6,5};
+const int MAX_FADER_VAL = 1024;
+
+const int motorUp = 12;
+const int motorDown = 13;
 
 int analogSliderValues[NUM_SLIDERS];
 int buttonValues[NUM_BUTTONS];
+int requestedMotorValue = INT16_MAX;
+uint nutralcounter = 0;
+int loopCounter = 0;
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
@@ -14,15 +30,61 @@ void setup() {
   for (int i = 0; i < NUM_BUTTONS; i++) {
     pinMode(buttonInputs[i], INPUT_PULLUP);
   }
-
+  pinMode(motorUp, OUTPUT);
+  pinMode(motorDown, OUTPUT);
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50);
   Serial.begin(9600);
 }
 
-void loop() {
-  updateSliderValues();
-  sendSliderValues(); // Actually send data (all the time)
-//   printSliderValues(); // For debug
-  delay(10);
+void adjustMotor() {
+  digitalWrite(motorDown, false);
+  digitalWrite(motorUp, false);
+  if (requestedMotorValue != INT16_MAX) {
+    strip.setPixelColor(0, 30, 0, 0);
+    strip.show();
+    if (analogSliderValues[0] >= 2 && analogSliderValues[0] <= 1022){
+      int diff = requestedMotorValue - analogSliderValues[0];
+      if (diff > HYSTERESIS)
+      {
+        Serial.printf("up: %d\n", diff);
+        digitalWrite(motorUp, true);
+        nutralcounter = 0;
+      }
+      else if (diff < -HYSTERESIS)
+      {
+        Serial.printf("down: %d\n", diff);
+        digitalWrite(motorDown, true);
+        nutralcounter = 0;
+      }
+      else
+      {
+        Serial.printf("nutral: %d\n", diff);
+          strip.setPixelColor(0, 0, 50, 0);
+          strip.show();
+        if (++nutralcounter == 20)
+        {
+          requestedMotorValue = INT16_MAX;
+          nutralcounter = 0;
+          strip.setPixelColor(0, 0, 0, 0);
+          strip.show();
+        }
+
+      }
+    }
+  }
+  
+}
+
+void receiveNewValues() {
+    if (Serial.available() > 0) {
+    String incomingString = Serial.readStringUntil('\n');
+    requestedMotorValue = incomingString.toInt();
+    // prints the received data
+    Serial.print("I received: ");
+    Serial.println(requestedMotorValue);
+    }
 }
 
 void updateSliderValues() {
@@ -58,7 +120,10 @@ void sendSliderValues() {
       builtString += String("|");
     }
   }
-
+  if(requestedMotorValue != INT16_MAX){
+    builtString += String("<<|>>");
+    builtString += String((int)requestedMotorValue);
+  }
   Serial.println(builtString);
 }
 
@@ -73,4 +138,17 @@ void printSliderValues() {
       Serial.write("\n");
     }
   }
+}
+
+void loop() {
+  adjustMotor();
+  if (++loopCounter == 10)
+  {
+    receiveNewValues();
+    updateSliderValues();
+    sendSliderValues(); // Actually send data (all the time)
+    //printSliderValues(); // For debug
+    loopCounter = 0;
+  }
+  delay(1);
 }
